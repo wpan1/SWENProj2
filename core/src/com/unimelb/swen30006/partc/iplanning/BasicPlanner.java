238714c86b713c8e414a9df1a0790663b404a1dc
wpan1 <wpan1@student.unimelb.edu.au>
2016-05-19 20:04:58 +1000

    modified planner

--- core/src/com/unimelb/swen30006/partc/iplanning/BasicPlanner.java
+++ core/src/com/unimelb/swen30006/partc/iplanning/BasicPlanner.java
@@ -1,6 +1,8 @@
 package com.unimelb.swen30006.partc.iplanning;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
+import java.util.ArrayList;
 
 import com.unimelb.swen30006.partc.ai.interfaces.IPlanning;
 import com.unimelb.swen30006.partc.ai.interfaces.PerceptionResponse;
@@ -11,6 +13,7 @@ public class BasicPlanner implements IPlanning{
 	PathGenerator pg;
 	PerceptionHandler pHandler;
 	Car c;
+	ArrayList<Point2D.Double> route;
 	
 	public BasicPlanner(Car c){
 		this.pg = new Dijkstra();
@@ -19,7 +22,9 @@ public class BasicPlanner implements IPlanning{
 	
 	@Override
 	public boolean planRoute(Double destination) {
-		return pg.findPath(c, destination);
+		if ((route = pg.findPath(c, destination)) != null)
+			return true;
+		return false;
 	}
 
 	@Override
